2d1cc3a7f596bc23a8429d0e2e0f0123b08ff5f0
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-11 20:22:08 +0100

    add voiture class

---
+++ src/Voiture.java
@@ -0,0 +1,26 @@
+public class Voiture extends Vehicule {
+    private int capacite;
+    public static int nbMaxVoiture = 10;
+    public static int nbVoitures = 0;
+
+    private Voiture(int modele) {
+        super(modele);
+    }
+
+    public void addCarburant(float c) {
+        if (this.getCarburant() + c < this.capacite) {
+            this.setCarburant(this.getCarburant() + c);
+        } else {
+            this.setCarburant(this.capacite);
+        }
+    }
+
+    public Voiture creeVoiture(int m) {
+        if (nbVoitures < nbMaxVoiture) {
+            Voiture voiture = new Voiture(m);
+            nbVoitures++;
+            return voiture;
+        }
+        return null;
+    }
+}
